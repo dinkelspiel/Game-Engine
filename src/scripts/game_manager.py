5b0439d1f0e743ac1004f1805f95f73a58b3c474
WillM <imdark200@gmail.com>
2021-05-25 09:34:42 +0200

    Game_Manager Comments
    added comments to Game_Manager.py

--- src/scripts/game_manager.py
+++ src/scripts/game_manager.py
@@ -1,4 +1,10 @@
+## Imports
+
+# Non Local Modules
+
 import pygame, os, json, pyautogui, sys, time
+
+# Local Modules
 
 from .modules.input import Input
 from .modules.language import Language_Handler
@@ -10,19 +16,13 @@ from .modules.renderer import renderer
 from .modules.fonts import font_handler
 from .modules.debug import Debug
 
-from .screens.screen_main import screen_main
-from .screens.screen_editor import screen_editor
+# Screens
+
 from .screens.gui_test import gui_test
 
+## Game Manager Class
+
 class Game_Manager:
-
-    game_speed = 1
-    clock = pygame.time.Clock()
-
-    running = True
-    title = "Game"
-
-    current_assetpack = "main"
 
     def stop(self):
         self.running = False
@@ -32,6 +32,12 @@ class Game_Manager:
         pygame.display.set_caption(title)
 
     def __init__(self):
+        ## Set asset pack (Needs to be done before modules)
+
+        self.current_assetpack = "main"
+
+        ## Initialize all modules
+
         self.input = Input()
         self.language_handler = Language_Handler(self)
         self.color_handler = Color_Handler(self)
@@ -42,9 +48,15 @@ class Game_Manager:
         self.font_handler = font_handler(self)
         self.debug = Debug(self)
 
-        self.previous_time = time.time()
+        ## Time
 
-        self.input.set_input_state('general')
+        self.game_speed = 1
+        self.clock = pygame.time.Clock()
+
+        ## General Info and Data
+
+        self.running = True
+        self.title = "Game"
 
         with open(os.path.join("src", "properties.json"), "r") as file:
             self.properties = json.load(file)
@@ -53,32 +65,47 @@ class Game_Manager:
         icon = pygame.image.load(os.path.join("src", "resources", self.current_assetpack, "assets", "icon.png"))
         pygame.display.set_icon(icon)
 
+        ## Start debug 
+
+        self.previous_time = time.time()
+
+        ## Set input state
+
+        self.input.set_input_state('general')
+
+    ## Set to default settings
+
     def initialize(self):
         self.change_title(self.properties["id"])
         self.input.input_state = "general"
 
-        main_screen = screen_main(self)
-        screen_gui_test = gui_test(self)
-        editor_screen = screen_editor(self)
-        self.renderer.load_screen(screen_gui_test, 'gui_test')
-        self.renderer.load_screen(editor_screen, 'editor')
-        self.renderer.load_screen(main_screen, 'main')
-        self.renderer.switch_screen('editor')
+    ## Main Update Method
 
     def update(self):
-        # Calculate Delta Time
+        ## Calculate Delta Time
+
         now = time.time()
         self.delta_time = now - self.previous_time
         self.previous_time = now
+
+        ## Input
 
         self.input.any_key_pressed = False
         for i, item in enumerate(self.input.pressed_keys):
             self.input.pressed_keys[i] = False
 
+        ## Events
+
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
+            
+            # Stop if quit called
+
             if event.type == pygame.QUIT:
                 self.stop()
+
+            # Input 
+
             if event.type == pygame.KEYDOWN:
                 self.input.any_key_pressed = True
                 for i, key in enumerate(self.input.record_keys):
@@ -86,17 +113,24 @@ class Game_Manager:
                     exec("key_signature = pygame.K_" + key)
                     self.input.pressed_keys[i] = keys[key_signature]
                         
-        # Screens
+        ## Screens
+
         self.renderer.update()
 
         self.debug.update()
+
+    ## Trail update method
 
     def end_update(self):
         self.input.check_keys()
         self.input.mouse_button_last_frame = self.input.is_mouse_button_pressed()
 
+    ## Main Renderer Method
+
     def render(self):  
         self.renderer.render()
+
+    ## Trail renderer method
 
     def end_render(self):  
         pass
