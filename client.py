cc7b55400ac63498d50987f7878fc995330ba238
WillM <imdark200@gmail.com>
2021-05-25 09:38:57 +0200

    Comments Client.py

--- client.py
+++ client.py
@@ -1,11 +1,18 @@
+## Imports
+
 import pygame, time
 from src.scripts.game_manager import Game_Manager
+
+## Initialize Game_Manager
 
 game = Game_Manager()
 
 game.initialize()
 
+## While loop
+
 while game.running:
+
     ## Update
 
     update_start_time = time.time()
@@ -20,6 +27,10 @@ while game.running:
     game.end_render()
     render_end_time = time.time()
 
+    ## Send Debug Stats
+
     game.debug.send_stats(update_end_time - update_start_time, render_end_time - render_start_time)
+
+## Quit
 
 pygame.quit()
