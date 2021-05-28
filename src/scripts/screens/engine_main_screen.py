1993a8b32789e97df6d73f77570780c278dff006
WillM <imdark200@gmail.com>
2021-05-28 13:08:57 +0200

    Changed colors
    Removed residue colors in colors.json and readjusted colors in engine to complementing blacks with a little blue in them which makes the engine look alot better

--- src/scripts/screens/engine_main_screen.py
+++ src/scripts/screens/engine_main_screen.py
@@ -9,25 +9,27 @@ class engine_main_screen(Screen):
         self.right_panel = gui_rect(game)
         self.right_panel.set_pos(percentage_constraint(0.78), pixel_constraint(25), 1)
         self.right_panel.set_size(percentage_constraint(0.22), percentage_constraint(1), 1)
+        self.right_panel.set_color(self.game.color_handler.get_rgb('engine.background'), 2)
 
         self.left_panel = gui_rect(game)
         self.left_panel.set_pos(percentage_constraint(0), pixel_constraint(25), 1)
         self.left_panel.set_size(percentage_constraint(0.22), percentage_constraint(1), 1)
+        self.left_panel.set_color(self.game.color_handler.get_rgb('engine.background'), 2)
 
         self.top_panel = gui_rect(game)
         self.top_panel.set_pos(pixel_constraint(0), pixel_constraint(0), 1)
         self.top_panel.set_size(percentage_constraint(1), pixel_constraint(25), 1)
-        self.top_panel.set_color((100, 100, 100), 2)
+        self.top_panel.set_color(self.game.color_handler.get_rgb('engine.background_alt'), 2)
 
         self.bottom_panel = gui_rect(game)
         self.bottom_panel.set_pos(percentage_constraint(0.221), percentage_constraint(0.598), 1)
         self.bottom_panel.set_size(percentage_constraint(0.56), percentage_constraint(0.4), 1)
-        self.bottom_panel.set_color((100, 100, 100), 2)
+        self.bottom_panel.set_color(self.game.color_handler.get_rgb('engine.background_alt'), 2)
         
         self.screen_panel = gui_rect(game)
         self.screen_panel.set_pos(percentage_constraint(0.221), pixel_constraint(26), 2)
         self.screen_panel.set_size(aspect_constraint(1.7777), percentage_constraint(0.56), 2)
-        self.screen_panel.set_color((200, 0, 0), 2)
+        self.screen_panel.set_color((255, 0, 0), 2)
 
     def update(self):
         self.right_panel.update()
@@ -37,7 +39,6 @@ class engine_main_screen(Screen):
         self.screen_panel.update()
 
     def render(self):
-        self.game.renderer.screen.fill(self.game.color_handler.get_rgb('main_menu.background'))
         self.right_panel.render()
         self.left_panel.render()
         self.top_panel.render()
