7e8107c49b9096d09eb39d4576de335f2651ea24
WillM <imdark200@gmail.com>
2021-06-06 14:09:52 +0200

    sfd

--- src/scripts/game/player.py
+++ src/scripts/game/player.py
@@ -1,5 +1,5 @@
 from src.scripts.modules.entity import *
-import pygame, math, numpy
+import pygame, math
 
 class Player_Renderer_Component:
     def __init__(self, game, parent) -> None:
@@ -18,15 +18,20 @@ class Player_Renderer_Component:
         pass
 
     def render(self):
-        self.walking_animation_frame_counter += 100 * self.game.delta_time
-        if self.walking_animation_frame_counter >= 3:
+        if self.game.input.is_pressed(self.parent.fwd):
+            self.walking_animation_frame_counter += 100 * self.game.delta_time
+        if self.walking_animation_frame_counter >= 6:
             self.walking_animation_frame += 1
             self.walking_animation_frame_counter = 0
         if self.walking_animation_frame > 17:
             self.walking_animation_frame = 0
 
+        pygame.draw.circle(self.game.renderer.main_surface, self.parent.color2, (self.parent.camera_relative_x, self.parent.camera_relative_y), 34)
+        pygame.draw.circle(self.game.renderer.main_surface, self.parent.color, (self.parent.camera_relative_x, self.parent.camera_relative_y), 32)
+
         mx, my = pygame.mouse.get_pos()
-        self.parent.transform.direction = 180 - self.game.math.direction_between_points(self.parent.transform.x, self.parent.transform.y, mx, my) + 360
+        if self.game.input.is_pressed(self.parent.fwd):
+            self.parent.transform.direction = 180 - self.game.math.direction_between_points(self.parent.transform.x, self.parent.transform.y, mx, my) + 360
 
         image = self.walking_animation[self.walking_animation_frame]
         image = pygame.transform.scale(image, (64, 64))
@@ -39,15 +44,16 @@ class Player_Movement_Component():
         self.parent = parent
         
     def update(self):
-        move_speed = 400 * self.game.delta_time
+        move_acc = 50 * self.game.delta_time
+        self.parent.move_speed = min(self.parent.move_speed, 400 * self.game.delta_time)
+        if self.game.input.is_pressed(self.parent.fwd):
+            self.parent.move_speed += move_acc
+        else:
+            self.parent.move_speed -= self.parent.move_speed / 32
         mx, my = pygame.mouse.get_pos()
         angle = self.game.math.direction_between_points(self.parent.transform.x, self.parent.transform.y, mx, my)
-        if self.game.input.is_pressed('w'):
-            self.parent.transform.x += self.game.math.lengthdir_x(move_speed, angle)
-            self.parent.transform.y += self.game.math.lengthdir_y(move_speed, angle)
-
-
-
+        self.parent.transform.x += self.game.math.lengthdir_x(self.parent.move_speed, angle)
+        self.parent.transform.y += self.game.math.lengthdir_y(self.parent.move_speed, angle)
 
     def render(self):
         pass
@@ -59,6 +65,12 @@ class player(Entity):
 
         self.components = []
         self.transform = Transform()
+
+        self.move_speed = 0
+        self.fwd = 'w'
+
+        self.color = (238, 195, 154)
+        self.color2 = (238, 148, 127)
 
         self.add_component(Camera_Relative_Component(game, self))
         self.add_component(Player_Renderer_Component(game, self))
--- src/scripts/screens/game_screen.py
+++ src/scripts/screens/game_screen.py
@@ -3,21 +3,23 @@ from src.scripts.modules.gui import *
 import os
 
 from src.scripts.game.player import player
-from src.scripts.game.world import world
 
 class game_screen(Screen):
     def __init__(self, game):
         self.game = game
 
         self.player = player(game)
-        self.world = world(game)
+        self.player2 = player(game)
+        self.player2.fwd = 'UP'
+        self.player2.color = (153, 229, 80)
+        self.player2.color2 = (106, 190, 48)
 
     def update(self):
         self.player.update()
-        self.world.update()
+        self.player2.update()
 
     def render(self):
         self.game.renderer.main_surface.fill((0, 0, 0))
 
         self.player.render()
-        self.player.render()
+        self.player2.render()
